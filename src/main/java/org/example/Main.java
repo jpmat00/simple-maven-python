637668e3a82e68ef45a02e81f72ef74b71c79c44
João Matias <joao.matias@talkdesk.com>
2024-02-28 17:43:05 +0000

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import jep.Interpreter;
+import jep.SharedInterpreter;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Interpreter jep = new SharedInterpreter()) {
+            jep.eval("print('Hello, from Python!')");
+        }
+    }
+}
